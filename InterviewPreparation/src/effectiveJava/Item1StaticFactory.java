a5504845f22dc75ecf1677aee86cfd4ebbd6a999
pranay <pranayranga@gmail.com>
2013-02-19 00:15:35 -0500

    initial version

---
+++ InterviewPreparation/src/effectiveJava/Item1StaticFactory.java
@@ -0,0 +1,13 @@
+package effectiveJava;
+
+public class Item1StaticFactory {
+	
+	//immutable object 
+	public static final String NAME = "pranay";
+	//flyweight pattern
+	//instance controlled classes
+	// non-instantiable classes or singleton
+	//
+	
+
+}
