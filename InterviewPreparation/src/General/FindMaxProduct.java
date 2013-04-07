18f7431f53062781afe4f49da8b9f72e2d7ba209
Pranay Ranga <pranayranga@gmail.cmo>
2013-04-07 01:09:09 -0400

    http://www.careercup.com/question?id=16813665

---
+++ InterviewPreparation/src/General/FindMaxProduct.java
@@ -0,0 +1,42 @@
+package General;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMaxProduct
+{
+	
+	//TODO: check Arrays
+	//TODO: check collections
+	
+    private static List<Integer> input_ = new ArrayList<Integer>(Arrays.asList(6,7,8,1,2,3,9,10));    
+    private static List<Integer> prodList_ = new ArrayList<Integer>();
+    
+    public static void main(String[] args) 
+    {        
+        for(int i=0;i<input_.size();i++) {
+            int currentValue = input_.get(i);
+            //System.out.println(currentValue);
+            Collections.sort(prodList_);
+                        
+            if (prodList_.size() < 3) {
+                prodList_.add(currentValue);
+            }
+            else {
+                if (currentValue >= prodList_.get(0)) {
+                    prodList_.set(0, currentValue);
+                }
+            }
+        }
+        Collections.sort(prodList_);
+
+        for (int i=0;i<prodList_.size();i++) {
+            System.out.println(prodList_.get(i));
+        }
+    }	
+    
+}
+
+
---
+++ InterviewPreparation/src/General/FindMaxProductSequence.java
@@ -0,0 +1,44 @@
+package General;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindMaxProductSequence {
+	
+	public static List<Integer>	find(List<Integer> input)
+	{
+		List<Integer> prodList_ = new ArrayList<Integer>();
+		int maxProd = 1;
+		int index = 0;
+		
+		for (int i = 0 ; i < input.size()-2;i++)
+		{
+  		    if(maxProd < findProduct(i, input))
+			{
+				maxProd = findProduct(i, input);
+				index = i ;
+			}
+		}
+	   prodList_.add(input.get(index));
+	   prodList_.add(input.get(index+1));
+	   prodList_.add(input.get(index+2));
+		return prodList_;
+	}
+	
+	private static Integer findProduct(int i,List<Integer>input)
+	{
+		return input.get(i)*input.get(i+1)*input.get(i+2);
+	}
+	
+	
+	public static void main(String[] args) {
+	    List<Integer> input_ = new ArrayList<Integer>(Arrays.asList(6,7,8,1,2,3,11,9,10));    
+	    List<Integer> out = FindMaxProductSequence.find(input_);
+	    for(int i : out)
+	    {
+	    	System.out.println(i);
+	    }
+	}
+
+}
