25001659246b378c9427223080421385ec7d7ab5
pranay <pranayranga@gmail.com>
2013-04-10 00:49:31 -0400

    First Version of KeyCount program

--- src/com/dealer/KeyCount.java
+++ src/com/dealer/KeyCount.java
@@ -1,13 +1,49 @@
 package com.dealer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
 public class KeyCount {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Path path = FileSystems.getDefault().getPath(
+				"/Users/pranayranga/workspace/Practice/src/com/dealer/test",
+				"test.txt");
+		Map<String, Integer> nameMap = new HashMap<String, Integer>();
+
+		try {
+			BufferedReader reader = Files.newBufferedReader(path,
+					StandardCharsets.UTF_8);
+			String line;
+			
+			
+			while ((line = reader.readLine()) != null) {
+				String[] strArr = line.split(",");
+				int i = Integer.parseInt(strArr[1].trim());
+				String name = strArr[0].trim();
+				if (nameMap.get(name) != null) {
+					nameMap.put(name.trim(), nameMap.get(name) + i);
+				}
+				else
+				{  
+					nameMap.put(name,i);
+
+				}
+			
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (Map.Entry<String, Integer> entry : nameMap.entrySet())
+		{
+		    System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
 	}
-
 }
